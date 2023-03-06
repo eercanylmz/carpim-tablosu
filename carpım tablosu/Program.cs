39b5f13ab2d8be3e2916a626ee5ee176b51b89e0
Ercan Yılmaz <yilmazercan404@gmail.com>
2023-03-06 18:29:13 +0300

    Proje dosyası ekle.

---
+++ carpım tablosu/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpım_tablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı1, sayı2, sonuc;
+            string islem;
+            Console.WriteLine("1.sayıyı giriniz");
+            sayı1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("2. sayıyı giriniz");
+            sayı2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.WriteLine("işlem seçin= +,-,*,/");
+            
+            islem = Console.ReadLine();
+            Console.WriteLine("");
+            if (islem == "+")
+            {
+                sonuc = sayı1 + sayı2;
+                Console.WriteLine("sonuc= "+sonuc);
+            }
+            if (islem  == "-")
+            {
+                sonuc = sayı1 - sayı2;
+                Console.WriteLine("sonuc= " + sonuc);
+            }
+            if (islem == "*")
+            {
+                sonuc = sayı1 * sayı2;
+                Console.WriteLine("sonuc= " + sonuc);
+            }
+            if (islem == "/")
+            {
+                sonuc = sayı1 / sayı2;
+                Console.WriteLine("sonuc= " + sonuc);
+            }
+            Console.ReadLine();
+        }
+    }
+}
